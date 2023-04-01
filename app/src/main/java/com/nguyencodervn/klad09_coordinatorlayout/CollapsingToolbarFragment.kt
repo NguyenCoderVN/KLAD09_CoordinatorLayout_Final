7458dd81d5f2b236c383387bec0d3f79c95fbbce
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-04-01 08:24:30 +0700

    Commit

---
+++ app/src/main/java/com/nguyencodervn/klad09_coordinatorlayout/CollapsingToolbarFragment.kt
@@ -0,0 +1,6 @@
+package com.nguyencodervn.klad09_coordinatorlayout
+
+import androidx.fragment.app.Fragment
+
+
+class CollapsingToolbarFragment : Fragment(R.layout.fragment_collapsing_toolbar)
---
+++ app/src/main/java/com/nguyencodervn/klad09_coordinatorlayout/ScrollingToolbarFragment.kt
@@ -0,0 +1,6 @@
+package com.nguyencodervn.klad09_coordinatorlayout
+
+import androidx.fragment.app.Fragment
+
+
+class ScrollingToolbarFragment : Fragment(R.layout.fragment_scrolling_toolbar)
