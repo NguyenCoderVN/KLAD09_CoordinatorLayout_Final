541dfa84f6112a206963e2d824359a409df270b5
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-04-01 15:58:14 +0700

    commit

--- app/src/main/java/com/nguyencodervn/klad09_coordinatorlayout/CollapsingToolbarFragment.kt
+++ app/src/main/java/com/nguyencodervn/klad09_coordinatorlayout/CollapsingToolbarFragment.kt
@@ -1,6 +1,30 @@
 package com.nguyencodervn.klad09_coordinatorlayout
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
-class CollapsingToolbarFragment : Fragment(R.layout.fragment_collapsing_toolbar)
+class CollapsingToolbarFragment : Fragment(R.layout.fragment_collapsing_toolbar) {
+    private lateinit var materialToolbar: MaterialToolbar
+    private lateinit var floatingActionButton: FloatingActionButton
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        materialToolbar = view.findViewById(R.id.materialToolbar)
+        floatingActionButton = view.findViewById(R.id.FloatingActionButton)
+
+        (activity as AppCompatActivity).setSupportActionBar(materialToolbar)
+        floatingActionButton.setOnClickListener {
+            Toast.makeText(
+                context,
+                "Clicked FloatingActionButton",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+}
--- app/src/main/java/com/nguyencodervn/klad09_coordinatorlayout/ScrollingToolbarFragment.kt
+++ app/src/main/java/com/nguyencodervn/klad09_coordinatorlayout/ScrollingToolbarFragment.kt
@@ -1,6 +1,18 @@
 package com.nguyencodervn.klad09_coordinatorlayout
 
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.MaterialToolbar
 
 
-class ScrollingToolbarFragment : Fragment(R.layout.fragment_scrolling_toolbar)
+class ScrollingToolbarFragment : Fragment(R.layout.fragment_scrolling_toolbar) {
+    private lateinit var materialToolbar: MaterialToolbar
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        materialToolbar = view.findViewById(R.id.materialToolbar)
+
+        (activity as AppCompatActivity).setSupportActionBar(materialToolbar)
+    }
+}
