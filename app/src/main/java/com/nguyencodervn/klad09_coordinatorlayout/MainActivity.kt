0a40b6aa3584e4625ebb6e159af0312d65fa064e
NguyenCoderVN <41292507+NguyenCoderVN@users.noreply.github.com>
2023-04-01 10:49:23 +0700

    commit

--- app/src/main/java/com/nguyencodervn/klad09_coordinatorlayout/MainActivity.kt
+++ app/src/main/java/com/nguyencodervn/klad09_coordinatorlayout/MainActivity.kt
@@ -2,10 +2,26 @@ package com.nguyencodervn.klad09_coordinatorlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var navHostFragment : NavHostFragment
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
     }
 }
